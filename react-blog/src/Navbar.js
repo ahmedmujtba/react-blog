8bdd4af4dadad686ff4f138effed4cd2ee92442e
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2023-01-21 01:23:18 +0000

    initial component setup

---
+++ react-blog/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>This is Ahmed's Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/about">About me</a>
+        <a href="/myprojects">My Projects</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
