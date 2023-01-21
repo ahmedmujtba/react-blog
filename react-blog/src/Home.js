deb7949dd5bcab6d2d180773bd68d71a120fdf1e
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2023-01-21 22:10:54 +0000

    click events

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,5 +1,23 @@
 const Home = () => {
-  return <div className="home">Homepage</div>;
+  const handleClick = (e) => {
+    console.log("Ahmed says hello!", e.target);
+  };
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e);
+  };
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("john", e.target);
+        }}
+      >
+        Click me for a greeting
+      </button>
+    </div>
+  );
 };
 
 export default Home;
