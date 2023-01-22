0d75b1514ac825a423176911ad56eefe270f9362
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2023-01-22 12:29:30 +0000

    usestate practice

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,21 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("pikachu");
+  const [age, setAge] = useState(25);
+
   const handleClick = (e) => {
-    console.log("Ahmed says hello!", e.target);
+    setName("Ash");
+    setAge(30);
   };
-  const handleClickAgain = (name, e) => {
-    console.log("hello " + name, e);
-  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click Me</button>
-      <button
-        onClick={(e) => {
-          handleClickAgain("john", e.target);
-        }}
-      >
-        Click me for a greeting
-      </button>
+      <p>
+        {name} is {age} years old
+      </p>
     </div>
   );
 };
