a44bf95d01f09dda533974fed03cfc4963091afa
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2023-01-24 10:11:17 +0000

    outputting list - usestate

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,21 +1,36 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("pikachu");
-  const [age, setAge] = useState(25);
-
-  const handleClick = (e) => {
-    setName("Ash");
-    setAge(30);
-  };
-
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My blog project",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      author: "John Script",
+      id: 1,
+    },
+    {
+      title: "React tips for new devs",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      author: "Tom React",
+      id: 2,
+    },
+    {
+      title: "My favourite blog",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      author: "Jake Latin",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click Me</button>
-      <p>
-        {name} is {age} years old
-      </p>
+      {blogs.map((blog) => {
+        return (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written By: {blog.author}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
