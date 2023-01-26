7ab9b56016e6cebdeb939bc952f52c1a95e90b4b
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2023-01-26 15:27:24 +0000

    usefetch custom hook

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,23 +1,12 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const { blogs, isLoading, error } = useFetch("http://localhost:8000/blogs");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-        setIsLoading(false);
-      });
-  }, []);
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
---
+++ react-blog/src/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Sorry! Could not fetch the data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return { blogs, isLoading, error };
+};
+
+export default useFetch;
