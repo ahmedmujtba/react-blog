838cb690118388209412d4073e72bb9ca2217fe4
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2023-01-26 19:10:49 +0000

    link react-router-dom

--- react-blog/src/Navbar.js
+++ react-blog/src/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Ahmed Mujtba</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/about">About me</a>
-        <a href="/myprojects">My Projects</a>
-        <a href="/contact">Contact</a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About me</Link>
+        <Link to="/myprojects">My Projects</Link>
+        <Link to="/contact">Contact</Link>
       </div>
     </nav>
   );
